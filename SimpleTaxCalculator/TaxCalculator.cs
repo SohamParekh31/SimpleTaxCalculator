f6076e7896cb52169e26f75151c65c0ae999c359
SohamParekh31 <sohamparekh@promactinfo.com>
2020-08-13 11:54:20 +0530

    Initial Commit

--- SimpleTaxCalculator/TaxCalculator.cs
+++ SimpleTaxCalculator/TaxCalculator.cs
@@ -6,5 +6,12 @@ namespace SimpleTaxCalculator
 {
     class TaxCalculator
     {
+        private int Salary { get; set; }
+        private int Age { get; set; }
+        public TaxCalculator(int Salary,int Age)
+        {
+            this.Salary = Salary;
+            this.Age = Age;
+        }
     }
 }
